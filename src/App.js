3efa16496e982929ad30f43ebb3843461553bf32
rkr-abs <rkr@abstractit.in>
2023-04-10 15:43:00 +0530

    Add PDFViewer.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { PDFViewer, StyleSheet } from '@react-pdf/renderer';
 import { React } from 'react';
 import './App.scss';
 
+const styles = StyleSheet.create({
+	frame: {
+		width: '90vw',
+		height: '90vh',
+	},
+});
+
 const App = () =>
 	<div className="App">
-		Ready to start.
+		<PDFViewer style={ styles.frame }/>
 	</div>;
 
 export default App;
